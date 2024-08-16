225e023074222d3d9fffe8c93f30feedbcfdf2d5
Shohruh <shoh0554770876@gmail.com>
2024-08-16 15:42:35 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -12,17 +12,15 @@ class Eagle:
     def __init__(self):
         self.y_distance = 0
         self.sound = 'I train, eat, sleep, and repeat'
-        super().__init__()
 
     def fly(self, dy):
         self.y_distance += dy
 
 
-
 class Pegasus(Horse, Eagle):
     def move(self, dx, dy):
-        super().__init__() # Вызов метода run() класса Horse
-        super().__init__()  # Вызов метода fly() класса Eagle
+        super().run  # Вызов метода run() класса Horse
+        super().fly  # Вызов метода fly() класса Eagle
 
     def get_pos(self):
         return (self.x_distance, self.y_distance)
@@ -31,12 +29,12 @@ class Pegasus(Horse, Eagle):
         print(self.sound)
 
 
+pegaus = Pegasus()
+p2 = Eagle()
+print(pegaus.get_pos())
+pegaus.move(20, 15)
+print(pegaus.get_pos())
+pegaus.move(21, 20)
+print(pegaus.get_pos())
 
-p1 = Pegasus()
-print(p1.get_pos())
-p1.move(10, 15)
-print(p1.get_pos())
-p1.move(-5, 20)
-print(p1.get_pos())
-
-p1.voice()
+pegaus.voice()
